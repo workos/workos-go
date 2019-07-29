d9511ebd3dfb519adeef13fa0644afcba77d36fc
maxchehab <maxchehab@gmail.com>
2019-07-29 16:57:14 +0000

    remove: getting

--- auditlog/api.go
+++ auditlog/api.go
@@ -1,10 +1,7 @@
 package auditlog
 
 import (
-	"fmt"
-	"net/http"
 	"os"
-	"time"
 )
 
 const (
@@ -23,74 +20,4 @@ func init() {
 // SetAPIKey allows you to set the clients API key for all API requests.
 func SetAPIKey(key string) {
 	apiKey = key
-}
-
-// Get executes a get request to a provided resource
-func Get(path string) (*http.Response, error) {
-	client := http.Client{
-		Timeout: 10 * time.Second,
-	}
-
-	endpoint := os.Getenv("WORKOS_ENDPOINT")
-	if endpoint == "" {
-		endpoint = "https://api.workos.com"
-	}
-
-	route := fmt.Sprintf("%s%s", endpoint, path)
-	req, err := http.NewRequest("GET", route, nil)
-	if err != nil {
-		return nil, err
-	}
-
-	// Should error if not present
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("User-Agent", userAgent)
-	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", apiKey))
-
-	resp, err := client.Do(req)
-	if err != nil {
-		return nil, err
-	}
-
-	if resp.StatusCode < 200 && resp.StatusCode >= 300 {
-		return nil, fmt.Errorf("Received a %d", resp.StatusCode)
-	}
-
-	return resp, nil
-}
-
-// ListRequestParams allows you to configure FindAll or List request to paginate
-// any entries after & before a given index.
-type ListRequestParams struct {
-	After  string
-	Before string
-	Limit  int
-}
-
-func (p ListRequestParams) GetAfter() string {
-	if p.Before != "" && p.After != "" {
-		return ""
-	}
-	return p.After
-}
-
-func (p ListRequestParams) GetBefore() string {
-	return p.Before
-}
-
-func (p ListRequestParams) GetLimit() int {
-	if p.Limit > 1000 {
-		return 1000
-	}
-
-	if p.Limit <= 0 {
-		return 10
-	}
-
-	return p.Limit
-}
-
-type ListMeta struct {
-	HasMore    bool   `json:"has_more"`
-	TotalCount uint32 `json:"total_count"`
 }
--- auditlog/event.go
+++ auditlog/event.go
@@ -73,22 +73,6 @@ type Event struct {
 	idempotencyKey string
 }
 
-// EventResponse represents an Audit Log event stored in your WorkOS Audit Log.
-type EventResponse struct {
-	Event
-	ID              string `json:"id"`
-	Object          string `json:"object"`
-	LocationCity    string `json:"location_city"`
-	LocationState   string `json:"location_state"`
-	LocationCountry string `json:"location_country"`
-	AppID           string `json:"app_id"`
-}
-
-// GetID gets the Event's ID
-func (e EventResponse) GetID() string {
-	return e.ID
-}
-
 // NewEvent initializes a new event populated with default information about
 // the environment.
 func NewEvent(action Action, actionType ActionType) Event {
@@ -107,7 +91,7 @@ func NewEvent(action Action, actionType ActionType) Event {
 	}
 }
 
-// NewEventWithHTTP iniitalizes a new event populated with default information
+// NewEventWithHTTP initializes a new event populated with default information
 // about the environment and HTTP request.
 func NewEventWithHTTP(action Action, actionType ActionType, r *http.Request) Event {
 	event := NewEvent(action, actionType)
