21f9703428e58892152d75b1a0780dbf6354f3f2
Maxence Charriere <charriere@outlook.com>
2019-10-21 22:41:56 +0200

    update doc

--- pkg/auditlog/auditlog.go
+++ pkg/auditlog/auditlog.go
@@ -1,4 +1,23 @@
 // Package alog is a package to send audit logs events to WorkOS.
+//
+// Example:
+//   func main() {
+//       alog.SetAPIKey("my_api_key")
+//       defer alog.Close()
+//
+//       // Wherever you need to publish an audit log event:
+//       alog.Publish(alog.Event{
+//           Action:     "document.viewed",
+//           ActionType: "r",
+//           ActorName:  "Jairo Kunde",
+//           ActorID:    "user_01DGZ0FAXN978HCET66Q98QMTQ",
+//           Group:      "abstract.com",
+//           Location:   "55.27.223.26",
+//           OccurredAt: time.Now(),
+//           TargetName: "central.class",
+//           TargetID:   "doc_01DGZ0FAXP4HA4X0BVFKS0ZH4Y",
+//       })
+//   }
 package alog
 
 import (
