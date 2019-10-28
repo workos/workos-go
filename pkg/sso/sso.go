8b5fffb756c057a1b713bf0ecee75bcedb96af62
Maxence Charriere <charriere@outlook.com>
2019-10-28 15:15:44 +0100

    godoc example

--- pkg/sso/sso.go
+++ pkg/sso/sso.go
@@ -1,4 +1,34 @@
 // Package sso provide functions and client to communicate with WorkOS SSO API.
+//
+// Example:
+//   func main() {
+// 	     sso.SetAPIKey("my_api_key")
+//
+//       http.Handle("/login", sso.Login(sso.GetAuthorizationURLOptions{
+// 	         Domain:      "mydomain.com",
+// 	         ProjectID:   "my_workos_project_id",
+// 	         RedirectURI: "https://mydomain.com/callback",
+//       }))
+//
+// 	     http.HandleFunc("/callback", func(w http.ResponseWriter, r *http.Request) {
+// 	         profile, err := sso.GetProfile(context.Background(), sso.GetProfileOptions{
+// 	             Code:        r.URL.Query().Get("code"),
+// 	             ProjectID:   "my_workos_project_id",
+// 	             RedirectURI: "https://mydomain.com/callback",
+// 	         })
+// 	         if err != nil {
+// 	             // Handle the error ...
+// 	             return
+// 	         }
+//
+// 	         // Handle the profile ...
+// 	         fmt.Println(profile)
+//       })
+//
+//       if err := http.ListenAndServe("your_server_addr", nil); err != nil {
+//           panic(err)
+//       }
+//   }
 package sso
 
 import (
