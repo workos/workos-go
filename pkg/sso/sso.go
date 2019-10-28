7ceb4d7232f0a2dcb09f0f8370367145dd62ef78
Maxence Charriere <charriere@outlook.com>
2019-10-28 14:50:41 +0100

    handler + refactor names

--- pkg/sso/sso.go
+++ pkg/sso/sso.go
@@ -1,15 +1,50 @@
 // Package sso provide functions and client to communicate with WorkOS SSO API.
 package sso
 
+import (
+	"context"
+	"net/http"
+	"net/url"
+)
+
 const (
 	version = "0.0.1"
 )
 
-// ConnectionType represents a connection type.
-type ConnectionType string
-
-// Constants that enumerate the available connection types.
-const (
-	AzureSAML ConnectionType = "AzureSAML"
-	OktaSAML  ConnectionType = "OktaSAML"
+var (
+	// DefaultClient is the client used by SetAPIKey, GetAuthorizationURL,
+	// GetProfile and Login functions.
+	DefaultClient = &Client{}
 )
+
+// SetAPIKey set the api key to use with
+func SetAPIKey(apiKey string) {
+	DefaultClient.APIKey = apiKey
+}
+
+// GetAuthorizationURL returns an authorization url generated with the given
+// options.
+func GetAuthorizationURL(opts GetAuthorizationURLOptions) (*url.URL, error) {
+	return DefaultClient.GetAuthorizationURL(opts)
+}
+
+// GetProfile returns a profile describing the user that authenticated with
+// WorkOS SSO.
+func GetProfile(ctx context.Context, opts GetProfileOptions) (Profile, error) {
+	return DefaultClient.GetProfile(ctx, opts)
+}
+
+// Login return a http.Handler that redirects client to the appropriate
+// login provider.
+func Login(opts GetAuthorizationURLOptions) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		u, err := GetAuthorizationURL(opts)
+		if err != nil {
+			w.WriteHeader(http.StatusInsufficientStorage)
+			w.Write([]byte(err.Error()))
+			return
+		}
+
+		http.Redirect(w, r, u.String(), http.StatusSeeOther)
+	})
+}
