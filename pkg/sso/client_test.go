26052ff6b2d55a12c441588c1ab27aa5b2938b2d
Max Chehab <maxchehab@users.noreply.github.com>
2020-03-18 23:36:34 -0700

    Feature/refactor get profile to use form data (#40)
    * add form data

--- pkg/sso/client_test.go
+++ pkg/sso/client_test.go
@@ -142,7 +142,9 @@ func profileTestHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if clientSecret := r.URL.Query().Get("client_secret"); clientSecret != "test" {
+	r.ParseForm()
+
+	if clientSecret := r.Form.Get("client_secret"); clientSecret != "test" {
 		w.WriteHeader(http.StatusForbidden)
 		return
 	}
