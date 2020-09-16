5cea43def35dffd209b3713a115511b622b76c47
maxchehab <maxchehab@gmail.com>
2020-09-16 22:56:59 +0000

    tests

--- pkg/portal/client_test.go
+++ pkg/portal/client_test.go
@@ -117,3 +117,111 @@ func listOrganizationsTestHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Write(body)
 }
+
+func TestCreateOrganizations(t *testing.T) {
+	tests := []struct {
+		scenario string
+		client   *Client
+		options  CreateOrganizationsOpts
+		expected Organization
+		err      bool
+	}{
+		{
+			scenario: "Request without API Key returns an error",
+			client:   &Client{},
+			err:      true,
+		},
+		{
+			scenario: "Request returns Organization",
+			client: &Client{
+				APIKey: "test",
+			},
+			options: CreateOrganizationsOpts{
+				Name:    "Foo Corp",
+				Domains: []string{"foo-corp.com"},
+			},
+			expected: Organization{
+				ID:   "organization_id",
+				Name: "Foo Corp",
+				Domains: []OrganizationDomain{
+					OrganizationDomain{
+						ID:     "organization_domain_id",
+						Domain: "foo-corp.com",
+					},
+				},
+			},
+		},
+		{
+			scenario: "Request with duplicate Organization Domain returns error",
+			client: &Client{
+				APIKey: "test",
+			},
+			err: true,
+			options: CreateOrganizationsOpts{
+				Name:    "Foo Corp",
+				Domains: []string{"duplicate.com"},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.scenario, func(t *testing.T) {
+			server := httptest.NewServer(http.HandlerFunc(createOrganizationsTestHandler))
+			defer server.Close()
+
+			client := test.client
+			client.Endpoint = server.URL
+			client.HTTPClient = server.Client()
+
+			organization, err := client.CreateOrganization(context.Background(), test.options)
+			if test.err {
+				require.Error(t, err)
+				return
+			}
+			require.NoError(t, err)
+			require.Equal(t, test.expected, organization)
+		})
+	}
+}
+
+func createOrganizationsTestHandler(w http.ResponseWriter, r *http.Request) {
+	auth := r.Header.Get("Authorization")
+	if auth != "Bearer test" {
+		http.Error(w, "bad auth", http.StatusUnauthorized)
+		return
+	}
+
+	var opts CreateOrganizationsOpts
+	json.NewDecoder(r.Body).Decode(&opts)
+	for _, domain := range opts.Domains {
+		if domain == "duplicate.com" {
+			http.Error(w, "duplicate domain", http.StatusConflict)
+			return
+		}
+	}
+
+	if userAgent := r.Header.Get("User-Agent"); !strings.Contains(userAgent, "workos-go/") {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	body, err := json.Marshal(
+		Organization{
+			ID:   "organization_id",
+			Name: "Foo Corp",
+			Domains: []OrganizationDomain{
+				OrganizationDomain{
+					ID:     "organization_domain_id",
+					Domain: "foo-corp.com",
+				},
+			},
+		})
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(body)
+}
