064726c228d2b72a99f79463d9913fb0d53e250e
Max Chehab <maxchehab@users.noreply.github.com>
2020-09-16 15:37:43 -0700

    Feature/ch3304/add list organizations method to the go sdk (#53)

---
+++ pkg/portal/portal_test.go
@@ -0,0 +1,48 @@
+package portal
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/workos-inc/workos-go/pkg/common"
+)
+
+func TestPortalListOrganizations(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(listOrganizationsTestHandler))
+	defer server.Close()
+
+	DefaultClient = &Client{
+		HTTPClient: server.Client(),
+		Endpoint:   server.URL,
+	}
+	SetAPIKey("test")
+
+	expectedResponse := ListOrganizationsResponse{
+		Data: []Organization{
+			Organization{
+				ID:   "organization_id",
+				Name: "Foo Corp",
+				Domains: []OrganizationDomain{
+					OrganizationDomain{
+						ID:     "organization_domain_id",
+						Domain: "foo-corp.com",
+					},
+				},
+			},
+		},
+		ListMetadata: common.ListMetadata{
+			Before: "",
+			After:  "",
+		},
+	}
+
+	organizationsResponse, err := ListOrganizations(context.Background(), ListOrganizationsOpts{
+		Domains: []string{"foo-corp.com"},
+	})
+
+	require.NoError(t, err)
+	require.Equal(t, expectedResponse, organizationsResponse)
+}
