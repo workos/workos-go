ad51221102b76bdceeb36ba8f57e52570e9b711e
Taylor Hodge <j.taylor.hodge@gmail.com>
2021-01-28 14:06:27 -0500

    Add `GET /connections` (#64)

--- pkg/sso/client_test.go
+++ pkg/sso/client_test.go
@@ -10,6 +10,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	"github.com/workos-inc/workos-go/pkg/common"
 )
 
 func TestClientAuthorizeURL(t *testing.T) {
@@ -371,4 +372,109 @@ func createConnectionTestHandler(w http.ResponseWriter, r *http.Request) {
 
 	w.WriteHeader(http.StatusCreated)
 	w.Write(connection)
+}
+
+func TestListConnections(t *testing.T) {
+	tests := []struct {
+		scenario string
+		client   *Client
+		options  ListConnectionsOpts
+		expected ListConnectionsResponse
+		err      bool
+	}{
+		{
+			scenario: "Request without API Key returns an error",
+			client:   &Client{},
+			err:      true,
+		},
+		{
+			scenario: "Request returns Connections",
+			client: &Client{
+				APIKey: "test",
+			},
+			options: ListConnectionsOpts{},
+			expected: ListConnectionsResponse{
+				Data: []Connection{
+					Connection{
+						ID:                        "conn_id",
+						ConnectionType:            "GoogleOAuth",
+						Name:                      "Foo Corp",
+						OAuthRedirectURI:          "uri",
+						OAuthSecret:               "secret",
+						OAuthUID:                  "uid",
+						SamlEntityID:              "null",
+						SamlIDPURL:                "null",
+						SamlRelyingPartyTrustCert: "null",
+						SamlX509Certs:             []string{},
+						Status:                    "linked",
+					},
+				},
+				ListMetadata: common.ListMetadata{
+					Before: "",
+					After:  "",
+				},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.scenario, func(t *testing.T) {
+			server := httptest.NewServer(http.HandlerFunc(listConnectionsTestHandler))
+			defer server.Close()
+
+			client := test.client
+			client.Endpoint = server.URL
+			client.HTTPClient = server.Client()
+
+			connections, err := client.ListConnections(context.Background(), test.options)
+			if test.err {
+				require.Error(t, err)
+				return
+			}
+			require.NoError(t, err)
+			require.Equal(t, test.expected, connections)
+		})
+	}
+}
+
+func listConnectionsTestHandler(w http.ResponseWriter, r *http.Request) {
+	auth := r.Header.Get("Authorization")
+	if auth != "Bearer test" {
+		http.Error(w, "bad auth", http.StatusUnauthorized)
+		return
+	}
+
+	if userAgent := r.Header.Get("User-Agent"); !strings.Contains(userAgent, "workos-go/") {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	body, err := json.Marshal(ListConnectionsResponse{
+		Data: []Connection{
+			Connection{
+				ID:                        "conn_id",
+				ConnectionType:            "GoogleOAuth",
+				Name:                      "Foo Corp",
+				OAuthRedirectURI:          "uri",
+				OAuthSecret:               "secret",
+				OAuthUID:                  "uid",
+				SamlEntityID:              "null",
+				SamlIDPURL:                "null",
+				SamlRelyingPartyTrustCert: "null",
+				SamlX509Certs:             []string{},
+				Status:                    "linked",
+			},
+		},
+		ListMetadata: common.ListMetadata{
+			Before: "",
+			After:  "",
+		},
+	})
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(body)
 }
