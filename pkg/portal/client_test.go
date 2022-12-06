f241cd5eb85703ddb1d740e888cfa1b70bb042a9
Jônatas Santos <jonatascastro12@gmail.com>
2022-12-06 11:00:43 -0300

    Add  Admin Portal intent to Go SDK

--- pkg/portal/client_test.go
+++ pkg/portal/client_test.go
@@ -59,6 +59,18 @@ func TestGenerateLink(t *testing.T) {
 			},
 			expected: "https://id.workos.test/portal/launch?secret=1234",
 		},
+		{
+			scenario: "Request returns link with log_streams intent",
+			client: &Client{
+				APIKey: "test",
+			},
+			options: GenerateLinkOpts{
+				Intent:       LogStreams,
+				Organization: "organization_id",
+				ReturnURL:    "https://foo-corp.app.com/settings",
+			},
+			expected: "https://id.workos.test/portal/launch?secret=1234",
+		},
 	}
 
 	for _, test := range tests {
