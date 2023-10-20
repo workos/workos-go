ef371c839824d28c24b61731a08106a08b06fe87
Ash Godfrey <ashgodfrey@Ashs-MacBook-Pro.local>
2023-10-20 13:34:01 -0600

    add events

--- pkg/events/client.go
+++ pkg/events/client.go
@@ -42,6 +42,23 @@ func (c *Client) init() {
 		c.Endpoint = "https://api.workos.com"
 	}
 }
+ 
+
+const (
+	// Directory Events
+	DirectoryActivated  = "dsync.activated"
+	DirectoryDeleted    = "dsync.deleted"
+	// Directory User Events
+	DirectoryUserCreated = "dsync.user.created"
+	DirectoryUserUpdated = "dsync.user.updated"
+	DirectoryUserDeleted = "dsync.user.deleted"
+	// Directory Group Events
+	DirectoryGroupCreated = "dsync.group.created"
+	DirectoryGroupUpdated = "dsync.group.updated"
+	DirectoryGroupDeleted = "dsync.group.deleted"
+	DirectoryGroupUserAdded = "dsync.group.user_added"
+	DirectroyGroupUserRemoved = "dsync.group.user_removed"
+)
 
 // Event contains data about a particular Event.
 type Event struct {
