84a9662db6be4787178b08b5994efc6c46f34ab9
Rakesh Patel <roc@rocpatel.me>
2024-07-04 12:53:07 -0700

    Add oauthcredentials client

---
+++ pkg/oauthcredentials/oauthcredentials.go
@@ -0,0 +1,42 @@
+// Package `organizations` provides a client wrapping the WorkOS OAuthCredentials API.
+package oauthcredentials
+
+import (
+	"context"
+)
+
+// DefaultClient is the client used by SetAPIKey and OAuthCredentials functions.
+var (
+	DefaultClient = &Client{
+		Endpoint: "https://api.workos.com",
+	}
+)
+
+// SetAPIKey sets the WorkOS API key for OAuthCredentials requests.
+func SetAPIKey(apiKey string) {
+	DefaultClient.APIKey = apiKey
+}
+
+// GetOAuthCredential gets an OAuthCredential.
+func GetOAuthCredential(
+	ctx context.Context,
+	opts GetOAuthCredentialOpts,
+) (OAuthCredential, error) {
+	return DefaultClient.GetOAuthCredential(ctx, opts)
+}
+
+// ListOAuthCredentials gets a list of OAuthCredentials.
+func ListOAuthCredentials(
+	ctx context.Context,
+	opts ListOAuthCredentialsOpts,
+) (ListOAuthCredentialsResponse, error) {
+	return DefaultClient.ListOAuthCredentials(ctx, opts)
+}
+
+// UpdateOAuthCredential creates an OAuthCredential.
+func UpdateOAuthCredential(
+	ctx context.Context,
+	opts UpdateOAuthCredentialOpts,
+) (OAuthCredential, error) {
+	return DefaultClient.UpdateOAuthCredential(ctx, opts)
+}
