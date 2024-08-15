24655bc49db3acbe862dd63a772af751ec4a0324
Sherry Ali <sherry@workos.com>
2024-08-15 14:21:13 -0400

    Add certificate renewal intent

--- pkg/portal/client.go
+++ pkg/portal/client.go
@@ -67,6 +67,7 @@ const (
 	DSync      GenerateLinkIntent = "dsync"
 	AuditLogs  GenerateLinkIntent = "audit_logs"
 	LogStreams GenerateLinkIntent = "log_streams"
+	CertificateReneawl GenerateLinkIntent = "certificate_renewal"
 )
 
 // GenerateLinkOpts contains the options to request Organizations.
